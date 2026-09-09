03d7f0d896b2796a149b6adfbf3faee3f3c27229
Ashiiikkk <ashiknayem163@gmail.com>
2026-09-10 04:42:34 +0600

    Practice day-4

---
+++ Practice4.cpp
@@ -0,0 +1,34 @@
+#include <iostream>
+#include <algorithm>
+using namespace std;
+
+class Solution {
+public:
+    long long countCommas(long long n) {
+        long long ans = 0;
+        long long start = 1000;
+        long long commas = 1;
+
+        while (start <= n) {
+            long long end = min(n, start * 1000 - 1);
+
+            ans += (end - start + 1) * commas;
+
+            start *= 1000;
+            commas++;
+        }
+
+        return ans;
+    }
+};
+
+int main() {
+    Solution s;
+
+    long long n;
+    cin >> n;
+
+    cout << s.countCommas(n) << endl;
+
+    return 0;
+}
